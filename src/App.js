22fb2ca8d62908740fa028228b85589860817d6a
ZainSaleem1110 <szain0120@gmail.com>
2022-05-28 18:32:21 +0500

    navbar and banner section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BannerCard from './Component/banner'
+import Navbar from './Component/navbar'
+
+function App() {
+    return (
+        <div>
+            <Navbar/>
+            <BannerCard/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Component/banner.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function BannerCard() {
+    return (
+        <Box className="w-[100%] h-[530px] mt-14 banner flex justify-center items-center">
+            <Box className="w-[400px] mt-14 flex flex-col items-center">
+                    <Typography variant="h3" className="text-center">
+                        Make the next <span className="text-[#2066C7]">viral</span> meme.
+                    </Typography>
+                    <Typography variant="h6" sx={{mt:1}}>
+                        one does simply make cool memes.
+                    </Typography>
+                <CardActions className="mt-4">
+                    <Button variant="contained" className="btn">Start Making Memes</Button>
+                </CardActions>
+            </Box>
+        </Box>
+    );
+}
