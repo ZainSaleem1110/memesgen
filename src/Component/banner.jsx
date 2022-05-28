36c27544f9f678315a74714c1bef9a07b6fffc5c
ZainSaleem1110 <szain0120@gmail.com>
2022-05-28 18:41:25 +0500

    navbar and banner section

--- src/Component/banner.jsx
+++ src/Component/banner.jsx
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button';
 
 export default function BannerCard() {
     return (
-        <Box className="w-[100%] h-[530px] mt-14 banner flex justify-center items-center">
+        <Box className="w-[100%] h-[530px] mt-20 banner flex justify-center items-center">
             <Box className="w-[400px] mt-14 flex flex-col items-center">
                     <Typography variant="h3" className="text-center">
                         Make the next <span className="text-[#2066C7]">viral</span> meme.
