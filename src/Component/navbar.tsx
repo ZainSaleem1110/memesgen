39ed0be1b8a74be6b315828ab3456be309b29b28
ZainSaleem1110 <szain0120@gmail.com>
2022-05-29 21:35:35 +0500

    search bar

--- src/Component/navbar.tsx
+++ src/Component/navbar.tsx
@@ -11,13 +11,13 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 function Navbar() {
     return (
         <Card className="w-[100%] z-50 py-4 relative drop-shadow-lg flex justify-center items-center">
-            <Box className="flex sm:flex-row flex-col items-center sm:justify-between justify-center lg:px-10 px-5 w-[100%]">
+            <Box className="flex md:flex-row flex-col items-center md:justify-between justify-center lg:px-10 px-5 w-[100%]">
                 <img src={Logo} className="w-[190px]" alt="" />
-                    <Box className="lg:flex hidden items-center w-[400px] h-[44px] rounded-full bg-[#F1F1F1] text-[#AFAFAF] px-4 ml-[12%]">
+                    <Box className="flex items-center md:w-[400px] sm:w-[45%] w-[90%] h-[44px] rounded-full bg-[#F1F1F1] text-[#AFAFAF] px-4 md:ml-[12%] mx-[1%] md:mt-0 mt-5">
                         <SearchIcon />
                         <input type="text" name="search" id="" className="bg-[transparent] focus:outline-none w-[80%] ml-3" placeholder="Search" />
                     </Box>
-                    <Stack spacing={2} direction="row" className="sm:ml-[10%] sm:mt-0 mt-4">
+                    <Stack spacing={2} direction="row" className="md:ml-[10%] md:mt-0 mt-4">
                         <Button className="btn colorBtn w-[150px] h-[44px]">Create Meme</Button>
                         <Button className="btn outlineBtn w-[130px] h-[44px]" startIcon={<AccountCircleIcon />}>
                             Log In
