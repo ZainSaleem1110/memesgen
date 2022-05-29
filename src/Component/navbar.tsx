546fdd7a0bee50feb8bc116f3ad2e637f4a4211a
ZainSaleem1110 <szain0120@gmail.com>
2022-05-30 00:38:44 +0500

    search bar

--- src/Component/navbar.tsx
+++ src/Component/navbar.tsx
@@ -13,11 +13,11 @@ function Navbar() {
         <Card className="w-[100%] z-50 py-4 relative drop-shadow-lg flex justify-center items-center">
             <Box className="flex md:flex-row flex-col items-center md:justify-between justify-center lg:px-10 px-5 xl:w-[60%] lg:w-[80%] w-[90%]">
                 <img src={Logo} className="w-[190px]" alt="" />
-                    <Box className="flex items-center h-[44px] rounded-full bg-[#F1F1F1] text-[#AFAFAF] px-4 md:ml-[5%] md:mt-0 mt-5">
+                    <Box className="flex items-center lg:min-w-[300px] h-[44px] rounded-full bg-[#F1F1F1] text-[#AFAFAF] px-4 md:ml-[5%] md:mt-0 mt-5">
                         <SearchIcon />
                         <input type="text" name="search" id="" className="bg-[transparent] focus:outline-none w-[100%] ml-3" placeholder="Search" />
                     </Box>
-                    <Stack spacing={2} direction="row" className="md:ml-[10%] md:mt-0 mt-4">
+                    <Stack spacing={2} direction="row" className="ml-3 md:mt-0 mt-4">
                         <Button className="btn colorBtn w-[150px] h-[44px]">Create Meme</Button>
                         <Button className="btn outlineBtn w-[130px] h-[44px]" startIcon={<AccountCircleIcon />}>
                             Log In
