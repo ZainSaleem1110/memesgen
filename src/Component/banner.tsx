a56840f7946809cefe568e4a2ac1a733d6c4c884
ZainSaleem1110 <szain0120@gmail.com>
2022-06-01 10:10:36 +0500

    banner fixed

--- src/Component/banner.tsx
+++ src/Component/banner.tsx
@@ -8,13 +8,13 @@ import BannerMobImg from '../assest/bannerMobImg.svg'
 
 export default function BannerCard() {
     return (
-        <Box className="w-[100%] h-[80vh] relative flex justify-center items-center md:mt-0 mt-5 relative">
+        <Box className="w-[100%] md:h-[80vh] sm:h-[60vh] h-[30vh] relative flex justify-center items-center md:mt-0 mt-5 relative">
             <Box className="">
                 <img src={BannerImg} alt="" className="md:flex hidden" />
                 <img src={BannerMobImg} alt="" className="md:hidden flex" />
-                <Box className="w-[100%] sm:mt-14 flex flex-col items-center absolute lg:top-[100px] md:top-[80px] sm:top-[40px] top-[20px] left-0">
+                <Box className="w-[100%] mt-14 flex flex-col items-center absolute xl:top-[200px] lg:top-[100px] md:top-[90px] sm:top-[40px] top-[-50px] left-0">
                     <Typography className="text-center">
-                        <span className="lg:text-[56px] md:text-[32px] text-[20px] leading-tight text-[black] md:font-normal font-bold">Make the next <br /> <span className="text-[#2066C7]">viral</span> meme.</span>
+                        <span className="lg:text-[56px] md:text-[32px] text-[16px] leading-tight text-[black] md:font-normal font-bold">Make the next <br /> <span className="text-[#2066C7]">viral</span> meme.</span>
                     </Typography>
                     <Typography sx={{ mt: 1 }}>
                         one does simply make cool memes.
